b4830b7c67056b3d55b660581b4ac3713def5075
Raphael.Jesus <61888147+RaphaMenezesJ@users.noreply.github.com>
2021-01-31 18:27:25 -0300

    Calculator (VueJS) Application

---
+++ calculator-vue/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+        <h1>Calculator - VueJS</h1>
+        <Calculator />
+    </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator"
+
+export default {
+    components: {Calculator}
+}
+</script>
+
+<style>
+@font-face {
+    font-family: RobotoMono;
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+*{
+    font-family: "RobotoMono", monospace;
+}
+
+body{
+    margin: 0
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(90deg, rgba(0,42,42,1) 0%, rgba(26,187,190,1) 100%);
+}
+</style>
---
+++ calculator-vue/src/components/Display.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="display"
+       :class={historic} >
+        {{value}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {},
+        historic: { type: Boolean }
+    }
+}
+</script>
+
+<style>
+    .display{
+        grid-column: span 4;
+        background-color: #0004;
+
+        display: flex;
+        justify-content: flex-end;
+        align-items: flex-end;
+        padding: 8px;
+        font-size: 2.1em;
+        overflow: hidden;
+    }
+
+    .display.historic{
+        font-size: 1.3em
+    }
+</style>
